e1892ed05c5a3c647b0fe7baa69d7dc64680141a
Thirawat  Sinlapasomsak <96830223+thirawat27@users.noreply.github.com>
2023-10-04 16:01:32 +0700

    first commit

---
+++ output.js
@@ -0,0 +1,8 @@
+ 
+// คำสั่ง สำหรับ output ใน javascript
+
+ document.write("hello world"); // เป็นการแสดงโดยใช้ DOM จะปรากฏที่หน้าเว็บเพจ โดยจะปรากฏคำว้่า Hello world
+
+ console.log("hello world"); // จะแสดงตรง console debug ของ browser 
+
+ alert("hello world");  // เป็นการแสดงข้อความแบบก่ารแจ้งเตือน
